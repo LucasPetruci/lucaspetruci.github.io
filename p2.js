05d223200b9a5d203e51cba711857ae5d782beff
Lucas Sales Salvo Petruci <99514230+LucasPetruci@users.noreply.github.com>
2022-11-11 21:23:18 -0300

    p2

---
+++ p2.js
@@ -0,0 +1,41 @@
+function teste(){
+
+    
+}
+
+function pegar_dados(){
+    var valor = 0
+    var cidade_retirada = document.getElementsByName("cidade_retirada").value
+    var cidade_entrega = document.getElementsByName("cidade_entrega").value
+    var data_retirada = document.getElementById("data_retirada").value.split()
+    var data_entrega = document.getElementById("data_entrega").value.split()
+    let valor_carro = 0
+
+    if(document.getElementById("tipo_carro").value == "basico"){
+       valor += 119
+    }
+
+    if(document.getElementById("tipo_carro").value == "ar"){
+        valor += 199
+        
+     }
+
+     if(document.getElementById("tipo_carro").value == "executivo"){
+        valor += 299
+       
+     }
+
+     if(cidade_retirada.value != cidade_entrega.value){
+         valor = valor + 300
+    }
+   
+    if(document.getElementById("tipo_carro").value == "basico"){
+        alert("teste")
+    }
+    
+    if(data_retirada < data_entrega){
+        var resultado = data_entrega[0] - data_retirada[0]
+        valor = resultado * valor_carro
+    }
+    alert(valor)
+}
